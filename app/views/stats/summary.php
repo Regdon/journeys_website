f32f2381ac1ada41830a224eed668140f7c17534
= <marks83@hotmail.co.uk>
2023-04-19 20:34:17 +0100

    Added chart headers.

--- app/views/stats/summary.php
+++ app/views/stats/summary.php
@@ -4,10 +4,12 @@
     <div class = "col-md-6 mx-auto">
         <div class = "card card-body bg-light mt-5">
             <h2>Stats</h2>
+            <h3>Current Percentage Visited</h3>
             <p>Chart showing the percentage of stations which have been visited, along with the percentage only called at and the percentage not yet visited at all.</p>
             <p>Total Currently Visited: <?php echo $data['visited_summary_visited'] . ' / ' . $data['visited_summary_total_stations'] . ' (' . fmt_percent($data['visited_summary_percentage_visited']) . ')'; ?> </p>
             <div id='visted_summary' style='width: 100%; height: 500px;'></div>
 
+            <h3>Monthly New Stations</h3>
             <p>Chart showing the number of new stations visited each month for the past 12 months.</p>
             <p>Target stations per month: <?php echo $data['monthly_target']?>
             <p>Current 12 month total: <?php echo $data['twelve_month_count_new'];?>, an average of <?php echo fmt_int($data['twelve_month_average_new']);?> per month</p>
